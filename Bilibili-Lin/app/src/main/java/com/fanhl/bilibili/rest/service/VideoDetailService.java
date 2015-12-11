1ead540c53a92bf0846e23cf7158e4caa797e03b
fanhl <Dyguest0315>
2015-12-11 09:34:28 +0800

    videoDetail

--- Bilibili-Lin/app/src/main/java/com/fanhl/bilibili/rest/service/VideoDetailService.java
+++ Bilibili-Lin/app/src/main/java/com/fanhl/bilibili/rest/service/VideoDetailService.java
@@ -5,7 +5,7 @@ import retrofit.http.GET;
 /**
  * Created by fanhl on 15/12/11.
  */
-public interface DetailService {
+public interface VideoDetailService {
     @GET("http://api.bilibili.com/search_recommend?_device=android&_hwid=c315ca432ec47762&appkey=c1b107428d337928&build=408005&main_ver=v3&platform=android&playtag=3352228&recommend_type=related_post&rindex=1&sign=b78c0247fcaf1597613d4de2af9ed3c7")
     void videoDetial();
 }
